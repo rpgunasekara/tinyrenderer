2e57780de7e1c7e1aafbafb0b3433acb90d958d6
rpgunasekara <86504919+rpgunasekara@users.noreply.github.com>
2025-11-18 20:41:23 -0500

    Simple implementation of Bresenham's line algorithm.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "tgaimage.h"
+#include <cmath>
 
 constexpr TGAColor white = { 255, 255, 255, 255 }; // attention, BGRA order
 constexpr TGAColor green = { 0, 255,   0, 255 };
 constexpr TGAColor red = { 0,   0, 255, 255 };
 constexpr TGAColor blue = { 255, 128,  64, 255 };
 constexpr TGAColor yellow = { 0, 200, 255, 255 };
+
+// Given coordinates and a color, places pixels to form a line from a to b.
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    for (double t = 0.0; t <= 1.0; t+=0.02) {
+        int x = round(ax + t * (bx - ax));
+        int y = round(ay + t * (by - ay));
+        framebuffer.set(x, y, color);
+    }
+}
 
 int main(int argc, char** argv) {
     constexpr int width = 64;
@@ -18,6 +28,12 @@ int main(int argc, char** argv) {
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
     framebuffer.set(cx, cy, white);
+
+    // Making the lines from point to point.
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, bx, by, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
